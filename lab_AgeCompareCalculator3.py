f2d62eaa16a87969823199cb1b1bd16f6cad656d
eviatar21-meet <eviatar21@meet.mit.edu>
2019-07-23 09:57:54 +0300

    Stopping here

---
+++ lab_AgeCompareCalculator3.py
@@ -0,0 +1,6 @@
+p1 = int(input ('What is the age of the first person?'))
+p2 = int(input ('What is the age of the second person?'))
+age_dif = p1-p2
+age_dif = abs(age_dif)
+age_dif = str(age_dif)
+print("The age difference between the first person and the second person is " + age_dif + " years")
